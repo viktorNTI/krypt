82666cc4d00a9fae135f4732e09e0eccb080233b
viktorNTI <43564374+viktorNTI@users.noreply.github.com>
2018-12-04 10:28:10 +0100

    ninth commit

--- krypt.py
+++ krypt.py
@@ -3,6 +3,8 @@
 #algoritm: Instruktioner för hur problem/beräkningar ska lösas inom ett visst antal steg, på samma sätt varje gång.
 #säkerhet: Det är viktigt.
 
+import os, time
+
 word = " "
 key = 12
 letters = []
@@ -15,17 +17,27 @@ while meny !=3:
     except:
         print("Du måste ange antingen (1) eller (2)")
     if meny == 1:
+        os.system('cls')
+        f = open('log.txt', 'a+')
         word = (input("Mata in för kryptering: "))
+        f.write(word)
+        f.close()
         for letter in word:
             letters.append(ord(letter) + key)
         print(letters)
+        time.sleep(2)
         print(word)
         break
 
     elif meny == 2:
+        os.system('cls')
+        f = open('log.txt', 'a+')
         word = (input("Mata in för avkryptering: "))
+        f.write(word)
+        f.close()
         for l in word:
             crypt.append(ord(l) - key)
         print(crypt)
+        time.sleep(2)
         print(word)
         break
