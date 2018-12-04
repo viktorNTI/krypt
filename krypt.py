d3d5c4fd9387ffa31f447ff8d914ebdce9855d1c
viktorNTI <43564374+viktorNTI@users.noreply.github.com>
2018-12-04 09:11:44 +0100

    fourth commit

--- krypt.py
+++ krypt.py
@@ -1,14 +1,22 @@
-#kryptering:
-#hash:
-#algoritm:
+#kryptering: Kryptering används för att dölja information från alla som informationen inte är avsedd för. (Oftast med algoritmer.)
+#hash: Istället för att lagra lösenord i klartext så kan man spara en hash av lösenordet (oläslig) och sedan jämföra med den när du loggar in. 
+#algoritm: 
 #säkerhet:
 
 word = "test"
 key = 12
 letters = []
 crypt = []
+meny = 0
 
-
+while meny !=3:
+    try:
+        meny = int(input("Inmatning (1) Utmatning (2)"))
+    except:
+        print("Du måste ange antingen (1) eller (2)")
+    if meny == 1:
+        word = int(input("Mata in för kryptering: "))
+        
 
 for letter in word:
     letters.append(ord(letter) + key)
